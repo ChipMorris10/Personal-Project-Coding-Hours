93fae5caf7f65d1c863c8b1bd925333079b875be
ChipMorris10 <goldcomac@mac.com>
2015-08-14 15:06:08 -0600

    About to make major changes after talking with Reyna

--- js/main.js
+++ js/main.js
@@ -1,13 +1,58 @@
-// add scripts
+var count = 0;
+var startDate = '2015-07-06T09:30:46-0700';
+var endDate = new Date().toISOString();
+console.log(endDate);
 
 $(document).on('ready', function() {
-  console.log('sanity check!');
+    $.ajax({
+        url: "https://api.github.com/authorizations",
+        method:"POST",
+        data: JSON.stringify({
+            "client_id":config.client_id,
+            "client_secret":config.client_secret,
+            "note": "Personal Project",
+            "scopes": [
+            "public_repo"
+            ]
+        }),
+        dataType: "json",
+        headers:{
+            Authorization:"Basic " + config.basic_auth
+        }
+    }).done(function(data){
+        localStorage.setItem("token", data.token);
+        updateGithubCommits();
+    });
+    console.log('sanity check!');
 });
 
+function updateGithubCommits() {
+    $.ajax({
+        url: 'https://api.github.com/user/repos',
+        headers: {
+            Authorization: "token " + localStorage.getItem("token")
+        }
+    }).done(function(data) {
+        data.forEach(function(repo) {
+            countCommits(repo.name);
+        });
+        // COUNT IS SET
+        console.log(count);
+    });
+}
 
-// var hoursStudying = {
-//     hours: asd
-//     day of the week:
-//     date:
+function countCommits(repoName) {
+    $.ajax({
+        url: 'https://api.github.com/repos/ChipMorris10/' + repoName + '/commits?since=' + startDate + '&until=' + endDate,
+        headers: {
+            Authorization: "token " + localStorage.getItem("token")
+        },
+        async: false
+    }).done(function(data) {
+        count += data.length;
+    }).fail(function(data) {
+        console.log('no commits');
+    });
+}
 
-// };
+
